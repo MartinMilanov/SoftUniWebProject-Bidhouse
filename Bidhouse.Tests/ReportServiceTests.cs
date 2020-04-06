9aac0b04914616e16136c0e15d231702a00541a8
Martin Milanov <thelo@mail.bg>
2020-04-06 10:39:08 +0300

    Added Tests for Report and Review services

---
+++ Bidhouse.Tests/ReportServiceTests.cs
@@ -0,0 +1,124 @@
+﻿using Bidhouse.Models;
+using Bidhouse.Services.Posts;
+using Bidhouse.Services.Reports;
+using Bidhouse.Tests.Factories;
+using Bidhouse.ViewModels.PostModels;
+using Bidhouse.ViewModels.ReportModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Bidhouse.Tests
+{
+    public class ReportServiceTests
+    {
+        public ServiceFactory services { get; set; }
+        public IReportService reportService { get; set; }
+
+        public string postId { get; set; }
+        public string userId { get; set; }
+
+        public ReportServiceTests()
+        {
+            this.services = new ServiceFactory();
+            this.reportService = new ReportService(this.services.Context);
+            var postService = new PostService(this.services.Context);
+
+            var reportedUser = new User()
+            {
+                UserName = "ReportedUser"
+            };
+
+            var reportingUser = new User()
+            {
+                UserName = "ReportingUser"
+            };
+
+            this.services.UserManager.CreateAsync(reportedUser, "123456789").Wait();
+            this.services.UserManager.CreateAsync(reportingUser, "123456789").Wait();
+
+            var input = new CreatePostInputModel()
+            {
+                Name = "Test",
+                Price = 2,
+                Category = Category.Design,
+                Time = DateTime.Now,
+                Description = " afkjafskajfakjfsaksfjgaksfjgafksjgafkj",
+                Location = "Sofia"
+            };
+
+            this.userId = reportingUser.Id;
+            this.postId = postService.CreatePost(input, reportedUser.Id).Result;
+
+        }
+
+        [Fact]
+        public async Task AddReportShouldCreateReport()
+        {
+            var input = new ReportInputModel()
+            {
+                Description = "Description",
+                PostId = this.postId,
+                ReportType = ReportType.Crime
+            };
+
+            var result = await this.reportService.AddReport(userId, input);
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task DeleteReportShouldDeleteReport()
+        {
+            var input = new ReportInputModel()
+            {
+                Description = "Description",
+                PostId = this.postId,
+                ReportType = ReportType.Crime
+            };
+
+            var reportId = await this.reportService.AddReport(userId, input);
+
+            var result = await this.reportService.DeleteReport(reportId);
+
+            Assert.True(result == "Successfully removed report");
+        }
+
+        [Fact]
+        public async Task GetReportShouldReturnReport()
+        {
+            var input = new ReportInputModel()
+            {
+                Description = "Description",
+                PostId = this.postId,
+                ReportType = ReportType.Crime
+            };
+
+            var reportId = await this.reportService.AddReport(userId, input);
+
+            var result = await this.reportService.GetReport(reportId);
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task GetReportsShouldReturnReports()
+        {
+            var input = new ReportInputModel()
+            {
+                Description = "Description",
+                PostId = this.postId,
+                ReportType = ReportType.Crime
+            };
+
+            await this.reportService.AddReport(userId, input);
+
+            var result = await this.reportService.GetReports();
+
+            Assert.NotNull(result);
+        }
+
+    }
+}
---
+++ Bidhouse.Tests/ReviewServiceTests.cs
@@ -0,0 +1,72 @@
+﻿using Bidhouse.Models;
+using Bidhouse.Services.Reviews;
+using Bidhouse.Services.Users;
+using Bidhouse.Tests.Factories;
+using Bidhouse.ViewModels.ReviewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Bidhouse.Tests
+{
+    public class ReviewServiceTests
+    {
+        public ServiceFactory services { get; set; }
+        public IReviewService reviewService { get; set; }
+        public ReviewServiceTests()
+        {
+            this.services = new ServiceFactory();
+            var userService = new UserService(this.services.UserManager);
+            this.reviewService = new ReviewService(this.services.Context, userService);
+        }
+        [Fact]
+        public async Task PostReviewShouldPostReview()
+        {
+            var reviewedUser = new User()
+            {
+                UserName = "ReviewedUser"
+            };
+            var reviewingUser = new User()
+            {
+                UserName = "ReviewingUser"
+                
+            };
+
+           await this.services.UserManager.CreateAsync(reviewedUser,"12345678");
+           await this.services.UserManager.CreateAsync(reviewingUser, "12345678");
+               
+
+            var input = new ReviewInputModel()
+            {
+                Description = "Review",
+                Rating = 5,
+                ReviewedUserId = reviewedUser.Id
+            };
+
+           var result = await this.reviewService.PostReview(input, reviewingUser.Id);
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task PostReviewShouldReturnNull()
+        {
+        
+
+            var input = new ReviewInputModel()
+            {
+                Description = "Review",
+                Rating = 5,
+                ReviewedUserId = ""
+            };
+
+            var result = await this.reviewService.PostReview(input, "");
+
+            Assert.Null(result);
+        }
+
+    }
+}
